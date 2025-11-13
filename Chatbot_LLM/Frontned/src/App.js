0982fb24722fcba2a4b15815209f343f7358b1f4
Trina-SE <sultanatrina4209@gmail.com>
2025-11-13 14:39:49 +0600

    Organize

---
+++ Chatbot_LLM/Frontned/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatbotMessenger from './components/ChatbotMessenger';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatbotMessenger />
+    </div>
+  );
+}
+
+export default App;
